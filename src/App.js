0669f453c2ac65f19405c9a36b321012ea7306b6
Javier Marin <jjmarin777@gmail.com>
2021-08-01 17:30:08 -0500

    logic somewhat fixed

--- src/App.js
+++ src/App.js
@@ -1,51 +1,4 @@
-<<<<<<< HEAD
-import './App.css';
-import CardGameBoard from './components/CardGameBoardRCC'
-import React, { Component } from 'react'
 
-import Home from './Home';
-import { createDeckAndDraw } from './components/api';
-
-
-
-
-
-export class App extends Component {
-
-
-  state = {
-    currentCard1: [],
-    currentCard2: [],
-    deckId: null,
-    cards: [],
-    cards2: []
-  }
-  async componentDidMount() {
-    const { cards, cards2, deckId, } = await createDeckAndDraw();
-    this.setState({
-      deckId,
-      cards: cards,
-      cards2: cards2,
-      currentCard1: cards[0],
-      currentCard2: cards2[0]
-
-    })
-
-  }
-  render() {
-    return (
-      <div>
-        <div className="App">
-          <video src='/videos/video-1.mp4' autoPlay loop muted />
-          <Home />
-          <div>
-            <CardGameBoard currentCard1={this.state.currentCard1} currentCard2={this.state.currentCard2} player1Cards={this.state.cards} player2Cards={this.state.cards2} />
-          </div>
-        </div>
-      </div>
-    )
-  }
-=======
 import "./App.css";
 import CardGameBoard from "./components/CardGameBoard2";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -57,12 +10,12 @@ function App() {
       <video src='/videos/video-1.mp4' autoPlay loop muted />
       <Home />
       <div>
-        <CardGameBoard/>
+        <CardGameBoard />
       </div>
     </div>
 
   );
->>>>>>> b3739897cd361015f73fade488b63de3acfb5186
+
 }
 
 export default App
--- src/components/CardGameBoard2.js
+++ src/components/CardGameBoard2.js
@@ -11,6 +11,8 @@ export default class CardGameBoardRCC extends Component {
     cards2: [],
     discardPile1: [],
     discardPile2: [],
+    // viewCards: false,
+
   };
   async componentDidMount() {
     const { cards, cards2, deckId } = await createDeckAndDraw();
@@ -18,10 +20,13 @@ export default class CardGameBoardRCC extends Component {
       deckId,
       cards: cards,
       cards2: cards2,
+      currentCard1: cards[0],
+      currentCard2: cards2[0]
     });
-  }
+  } 0.
 
   compareCardValues = (player1Card, player2Card) => {
+
     const cardValues = [
       "2",
       "3",
@@ -39,31 +44,44 @@ export default class CardGameBoardRCC extends Component {
       "JOKER",
     ];
 
-// logic for comparing values and pushing winner cards into discardpile.
-    if (player1Card > player2Card) {
+
+    const player1CardValueIndex = cardValues.indexOf(player1Card.value);
+
+
+    const player2CardValueIndex = cardValues.indexOf(player2Card.value);
+
+
+    // logic for comparing values and pushing winner cards into discardpile.
+    if (player1CardValueIndex > player2CardValueIndex) {
       this.state.discardPile1.push(player1Card, player2Card);
       this.state.cards.splice(0, 1);
-
-    } else if (player2Card > player1Card) {
-      this.state.discardPile2.push(player2Card, player1Card);
       this.state.cards2.splice(0, 1);
+
+
+
+    } else if (player2CardValueIndex > player1CardValueIndex) {
+      this.state.discardPile2.push(player2Card, player1Card);
+
+      this.state.cards2.splice(0, 1);
+      this.state.cards.splice(0, 1);
+
     }
     console.log(this.state.discardPile1, this.state.discardPile2);
+    console.log(this.state.cards, this.state.cards2)
 
 
-    if (player2Card > player1Card) {
-      this.state.cards.splice(0, 1);
-    } else if (player1Card > player2Card) {
-      this.state.cards2.splice(0, 1);
-    }
 
-// Tie cards
-    if (player1Card === player2Card) {
+    // Tie cards
+    if (player1CardValueIndex === player2CardValueIndex) {
       this.state.discardPile1.push(player1Card);
+      this.state.cards.splice(0, 1);
+
       this.state.discardPile2.push(player2Card);
+      this.state.cards2.splice(0, 1);
+
     }
 
- // reshuffle
+    // reshuffle
     if (this.state.cards.length === 0) {
       this.state.cards.push(this.state.discardPile1);
     } else if (this.state.cards2.length === 0) {
@@ -73,7 +91,7 @@ export default class CardGameBoardRCC extends Component {
     // if (this.state.discardPile1 & this.state.discardPile2 >= 54){
     // }
 
-    console.log(this.state.cards, this.state.cards2);
+    // console.log(this.state.cards, this.state.cards2);
 
   };
 
@@ -82,18 +100,25 @@ export default class CardGameBoardRCC extends Component {
 
 
   drawCard = () => {
-    let cards = this.state.cards;
-    let cards2 = this.state.cards2;
 
-    i++;
 
-    this.setState({ currentCard1: cards[i] });
-    this.setState({ currentCard2: cards2[i] });
+    // this.setState({ viewCards: true });
+    this.compareCardValues(this.state.cards[i], this.state.cards2[i]);
+    this.setState({ currentCard1: this.state.cards[i], currentCard2: this.state.cards2[i] })
 
-    this.compareCardValues(
-      this.state.cards[i].value,
-      this.state.cards2[i].value,
-    );
+
+    // console.log(i)
+
+    // this.compareCardValues(this.state.cards[i], this.state.cards2[i]);
+    // this.setState({ currentCard1: this.state.cards[i], currentCard2: this.state.cards2[i] })
+
+
+    // console.log(i)
+    if (this.state.cards.length == 1 || this.state.cards2.length == 1) {
+      console.log('out of cards')
+    }
+
+
   };
 
 
@@ -101,9 +126,10 @@ export default class CardGameBoardRCC extends Component {
   render() {
     return (
       <div>
+        {/* {this.state.viewCards ? <img src={this.state.currentCard1.image} alt="your card" /> : null}
+        {this.state.viewCards ? <img src={this.state.currentCard2.image} alt="your card" /> : null} */}
         <img src={this.state.currentCard1.image} alt="your card" />
         <img src={this.state.currentCard2.image} alt="your card" />
-
         <button onClick={this.drawCard}>draw</button>
       </div>
     );
