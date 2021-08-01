3fb1b38d9b2759a09eec76fae7ca63dea17ff723
Javier Marin <jjmarin777@gmail.com>
2021-08-01 14:33:04 -0500

    some logic done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import './App.css';
 import CardGameBoard from './components/CardGameBoardRCC'
+import React, { Component } from 'react'
+
 import Home from './Home';
+import { createDeckAndDraw } from './components/api';
 
 
 
-function App() {
-  return (
-    <div className="App">
-          <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <Home />
+
+
+export class App extends Component {
+
+
+  state = {
+    currentCard1: [],
+    currentCard2: [],
+    deckId: null,
+    cards: [],
+    cards2: []
+  }
+  async componentDidMount() {
+    const { cards, cards2, deckId, } = await createDeckAndDraw();
+    this.setState({
+      deckId,
+      cards: cards,
+      cards2: cards2,
+      currentCard1: cards[0],
+      currentCard2: cards2[0]
+
+    })
+
+  }
+  render() {
+    return (
       <div>
-      <CardGameBoard/>
+        <div className="App">
+          <video src='/videos/video-1.mp4' autoPlay loop muted />
+          <Home />
+          <div>
+            <CardGameBoard currentCard1={this.state.currentCard1} currentCard2={this.state.currentCard2} player1Cards={this.state.cards} player2Cards={this.state.cards2} />
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
-
+export default App
--- src/components/CardGameBoardRCC.js
+++ src/components/CardGameBoardRCC.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react'
-import { createDeckAndDraw } from './api'
+import { createDeckAndDraw } from './api';
+import Cards from './Cards'
 
 let i = 0;
 export default class CardGameBoardRCC extends Component {
+
 	state = {
-		currentCard1: [],
-		currentCard2: [],
-		deckId: null,
-		cards: [],
-		cards2: []
+		currentCard1: {},
+		currentCard2: {},
+		viewCards: false,
+		player1Score: 0,
+		player2Score: 0
 	}
-	async componentDidMount() {
-		const { cards, cards2, deckId, } = await createDeckAndDraw();
-		this.setState({
-			deckId,
-			cards: cards,
-			cards2: cards2
-		})
+
+
+
+
+
+
+	drawCard = () => {
+		if (i === 0) {
+
+			this.setState({ viewCards: true });
+			this.compareCardValues(this.props.player1Cards[i], this.props.player2Cards[i]);
+			this.setState({ currentCard1: this.props.player1Cards[i], currentCard2: this.props.player2Cards[i] })
+
+			i++
+			// console.log(i)
+
+		} else if (i < this.props.player1Cards.length) {
+			this.compareCardValues(this.props.player1Cards[i], this.props.player2Cards[i]);
+			this.setState({ currentCard1: this.props.player1Cards[i], currentCard2: this.props.player2Cards[i] })
+
+
+			i++
+			// console.log(i)
+
+		}
+		else
+			console.log('out of cards')
+
+
 
 	}
 
-	compareCardValues = (player1Card) => {
-		console.log(player1Card)
+
+
+
+
+	compareCardValues = (player1Card, player2Card) => {
+		let player1Score = this.state.player1Score;
+		let player2Score = this.state.player2Score;
 		const cardValues = [
 			'2',
 			'3',
@@ -41,68 +70,37 @@ export default class CardGameBoardRCC extends Component {
 
 
 		// const previousCardValueIndex = cardValues.indexOf(previousCardValue);
-		const player1CardValueIndex = cardValues.indexOf(player1Card);
-		console.log(player1Card)
+		const player1CardValueIndex = cardValues.indexOf(player1Card.value);
 
-		// const player2CardValueIndex = cardValues.indexOf(player2Card);
+
+		const player2CardValueIndex = cardValues.indexOf(player2Card.value);
 
 
 		// if (previousCardValueIndex === -1 || currentCardValueIndex === -1) {
 		// 	throw new Error('Supplied cardValue not found in cardValues array');
 		// }
 
-		// if (bet === 'up' && previousCardValueIndex >= currentCardValueIndex) {
-		// 	return 'Lose';
-		// }
-		// if (bet === 'up' && previousCardValueIndex < currentCardValueIndex) {
-		// 	return 'Win';
-		// }
-
-		// if (bet === 'down' && previousCardValueIndex <= currentCardValueIndex) {
-		// 	return 'Lose';
-		// }
-		// if (bet === 'down' && previousCardValueIndex > currentCardValueIndex) {
-		// 	return 'Win';
-		// }
+		if (player1CardValueIndex > player2CardValueIndex) {
+			// return 'you win';
+			console.log('you win')
+			this.setState({ player1Score: player1Score + 1 })
+		}
+		if (player1CardValueIndex < player2CardValueIndex) {
+			// return 'you lose';
+			console.log('you lose')
+			this.setState({ player2Score: player2Score + 1 })
 
 
+		}
 
-
-	}
-
-
-	drawCard = () => {
-		let cards = this.state.cards
-		let cards2 = this.state.cards2
+		if (player1CardValueIndex == player2CardValueIndex) {
+			// return 'tie';
+			console.log('tie')
 
 
 
-		this.setState({ currentCard1: cards[i] });
-		this.setState({ currentCard2: cards2[i] });
-		i++;
+		}
 
-
-		// for (let i = 0; i < cards.length; i++) {
-
-
-		// 	this.setState({ currentCard1: cards[i] })
-
-		// 	break;
-
-
-		// }
-
-
-
-
-		// for (let i = 0; i < cards2.length; i++) {
-
-		// 	this.setState({ currentCard2: cards2[i] })
-
-
-		// 	break;
-
-		// }
 
 
 
@@ -112,8 +110,11 @@ export default class CardGameBoardRCC extends Component {
 	render() {
 		return (
 			<div>
-				<img src={this.state.currentCard1.image} alt="your card" />
-				<img src={this.state.currentCard2.image} alt="your card" />
+				<Cards />
+				{this.state.viewCards ? <img src={this.state.currentCard1.image} alt="your card" /> : null}
+				{this.state.viewCards ? <img src={this.state.currentCard2.image} alt="your card" /> : null}
+
+
 
 				<button onClick={this.drawCard}>draw</button>
 
@@ -121,3 +122,7 @@ export default class CardGameBoardRCC extends Component {
 		)
 	}
 }
+
+
+
+
